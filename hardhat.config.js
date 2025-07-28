01cc8ad3e8fa3258116f90fc1776bb8c1ba941f0
Hani Siddiqui <33376976+hanisiddiqui@users.noreply.github.com>
2025-07-27 22:37:14 -0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    plasmaTestnet: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
